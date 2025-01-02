5a95f62d03490e2cfc2b67515c4e53ed1f2d3b17
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2025-01-02 22:58:57 +0500

    msg

--- components/server.js
+++ components/server.js
@@ -456,7 +456,7 @@ app.get('/register/:id', async (req, res) => {
   }
 });
 
-app.get('/register/:userId', async (req, res) => {
+app.get('/user-register/:userId', async (req, res) => {
   try {
     const { userId } = req.params;
     const adminId = await AdminRegister.find({ userId: userId });
